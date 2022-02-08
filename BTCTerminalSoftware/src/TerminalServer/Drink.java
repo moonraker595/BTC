496526dc526fe33344d4f18bbf66e64a7d973d91
Alexander Kemp <alexander.kemp@stfc.ac.uk>
2022-02-08 13:23:35 +0000

    added getters and setters

--- BTCTerminalSoftware/src/TerminalServer/Drink.java
+++ BTCTerminalSoftware/src/TerminalServer/Drink.java
@@ -3,5 +3,21 @@ package TerminalServer;
 public class Drink {
     public String name;
     public double price;
-    public Integer stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
